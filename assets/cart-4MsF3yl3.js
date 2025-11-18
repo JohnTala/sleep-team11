6b8f71b86105ffb31d0ff6d350ca9689002428fe
JohnTala <57272492+JohnTala@users.noreply.github.com>
2025-11-19 01:04:19 +0200

    Updates

--- assets/cart-4MsF3yl3.js
+++ assets/cart-4MsF3yl3.js
@@ -1,4 +1,4 @@
-import"./style-HlQJNEhF.js";import{g as t}from"./utils-CHt_q3sJ.js";function e(){const r=t("so-cart").map(c=>s(c));document.querySelector(".product-list").innerHTML=r.join("")}function s(a){return`<li class="cart-card divider">
+import{g as t}from"./utils-kaFXJsRg.js";function e(){const r=t("so-cart").map(c=>s(c));document.querySelector(".product-list").innerHTML=r.join("")}function s(a){return`<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
       src="${a.Image}"
