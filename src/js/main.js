7cd96268ef6a196f48425b4cd81e603e54a5380e
JohnTala <57272492+JohnTala@users.noreply.github.com>
2025-11-19 23:01:20 +0200

    restore all the files

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,11 @@
-// import { loadHeaderFooter } from "./utils.mjs";
-// import ProductData from "./ProductData.mjs";
-// import ProductList from "./ProductList.mjs";
-
-// loadHeaderFooter();
-
-// const dataSource = new ProductData("tents");
-// const element = document.querySelector(".product-list");
-// const productList = new ProductList("Tents", dataSource, element);
-
-// productList.init();
-// import { loadHeaderFooter } from "./utils.mjs";
-// import ProductData from "./ProductData.mjs";
-// import ProductList from "./ProductList.mjs";
-
-// loadHeaderFooter();
-
-// const dataSource = new ProductData("tents");
-// const productListElement = document.querySelector(".product-list");
-
-// const productList = new ProductList("Tents", dataSource, productListElement);
-// productList.init();
-// src/js/index.js
-import { qs, loadHeaderFooter, renderListWithTemplate } from "./utils.mjs";
+//main.js linked to src/index.html
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-loadHeaderFooter();
-
 const dataSource = new ProductData("tents");
-const listElement = qs(".product-list");
-const productList = new ProductList("Tents", dataSource, listElement);
+
+const element = document.querySelector(".product-list");
+
+const productList = new ProductList("Tents", dataSource, element);
 
 productList.init();
