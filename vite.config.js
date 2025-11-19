b70d24c0130d25aeb321a5fa6062ebc2c6fa00f3
JohnTala <57272492+JohnTala@users.noreply.github.com>
2025-11-19 22:08:54 +0200

    Revert "reformatting all files"
    This reverts commit bc149cbfb3349c9d6494edb89e4f8942d315bc21.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
-   base:"/sleep-team11/",
+  base:"/sleep-team11/",
   build: {
     outDir: "../dist",
     rollupOptions: {
