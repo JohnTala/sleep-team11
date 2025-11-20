25f662bdb70bd79c625b66c53b68e13a17d62b3f
JohnTala <57272492+JohnTala@users.noreply.github.com>
2025-11-20 11:20:08 +0200

    Updates

--- assets/ProductData-BX6pX2b7.js
+++
@@ -1,1 +0,0 @@
-function e(o){if(o.ok)return o.json();throw new Error("Bad Response")}class r{constructor(t){this.category=t,this.path=`/sleep-team11/public/json/${this.category}.json`}getData(){return fetch(this.path).then(e).then(t=>t)}async findProductById(t){return(await this.getData()).find(s=>s.Id===t)}}export{r as P};
