98760bb5628cc608044170346add5a4d14609526
JohnTala <57272492+JohnTala@users.noreply.github.com>
2025-11-20 11:18:34 +0200

    reformatting files

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,16 +9,13 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    // public/json is copied to dist/public/json by Vite
-    this.path = `/sleep-team11/public/json/${this.category}.json`;
+    this.path = `../public/json/${this.category}.json`;
   }
-
   getData() {
     return fetch(this.path)
       .then(convertToJson)
       .then((data) => data);
   }
-
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
